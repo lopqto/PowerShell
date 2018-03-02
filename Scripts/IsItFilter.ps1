966bd363ef5a4bf7a4162c4ed36770aaa4094a2e
lopqto <morpix@protonmail.com>
2018-03-02 09:48:47 +0330

    add IsItFilter.ps1

---
+++ Scripts/IsItFilter.ps1
@@ -0,0 +1,13 @@
+﻿$InputFileName = Read-Host -Prompt "Please Enter File Name "
+
+foreach ($Domain in Get-Content "$InputFileName") {
+    $IP = $(Resolve-DnsName "$Domain").IPAddress | Select-Object -First 1
+    $Result = ""
+    If ( "$IP" -eq "10.10.34.34" ) {
+        $Result = "$Domain : Blocked"
+    }
+    Else {
+        $Result = "$Domain : Free"
+    }
+    Out-File -Append IsItFilterResult.txt -InputObject $Result
+}
